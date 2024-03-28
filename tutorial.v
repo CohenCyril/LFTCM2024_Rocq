0fed11a7b687c0ce91db1baf7af20193b5659e51
Cyril Cohen <cohen@crans.org>
2024-03-28 09:52:58 +0100

    Removing inexpected use of extended reals

--- tutorial.v
+++ tutorial.v
@@ -3,14 +3,12 @@ From mathcomp Require Import all_ssreflect ssralg ssrint ssrnum finmap matrix.
 From mathcomp Require Import rat interval zmodp vector fieldext falgebra.
 From mathcomp Require Import mathcomp_extra boolp classical_sets functions.
 From mathcomp Require Import cardinality set_interval Rstruct.
-From mathcomp Require Import ereal reals signed topology prodnormedzmodule normedtype.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
 Import Order.TTheory GRing.Theory Num.Theory.
-Import numFieldTopology.Exports.
 
 (* Demo File for the 9:00 - 10:15 lesson :Coq/Rocq Tutorial: ssreflect tactics
 and the MathComp library *)
@@ -285,6 +283,8 @@ by rewrite sqr_sqrtr// inE/=; exists 2 => //=; rewrite ratr_nat.
 Qed.
 
 (** Near  notations and tactics **)
+From mathcomp Require Import ereal reals signed topology prodnormedzmodule normedtype.
+Import numFieldTopology.Exports.
 
 Local Open Scope classical_set_scope.
 Local Open Scope ring_scope.
